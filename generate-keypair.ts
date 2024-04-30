ebc7c05b66300fc07efc68d54ddc1feffb0382a2
mrtewogbade <johndharey@gmail.com>
2024-04-30 15:18:41 +0100

    loaded keys

--- generate-keypair.ts
+++ generate-keypair.ts
@@ -1,11 +1,6 @@
-import { Keypair } from "@solana/web3.js";
-const keypair = Keypair.generate();
+import "dotenv/config";
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
 
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
 
-console.log(`The public key is: `, keypair.publicKey.toBase58());
-console.log(`The secret key is: `, keypair.secretKey);
-console.log(`Finished`);
-
-
-
-
+console.log(`Finished! We've loaded our secret key securely, using an env file`);
